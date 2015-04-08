da60d8270755c8da50a68fd64f1b7ba541d967a5
jonaslomholdt <jonaslomholdt@gmail.com>
2015-04-08 21:20:10 +0200

    Updatet LoginStatements with better JDBC handling

--- src/com/lomholdt/lagerstyring/model/LoginStatements.java
+++ src/com/lomholdt/lagerstyring/model/LoginStatements.java
@@ -1,6 +1,6 @@
 package com.lomholdt.lagerstyring.model;
 
-import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,13 +10,11 @@ import com.lomholdt.lagerstyring.model.User;
 public class LoginStatements extends DBMain {
 	    
     public boolean login(String username, String pwd, String companyName) {
+		
     	try {
-			pwd = Hash.hash256(pwd);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-    	try {
-    		pstmt = c.preparedStatement("SELECT users.username, users.password, companies.name AS company FROM users, companies WHERE username = ? AND companies.name = ?;");
+    		Connection connection = c.getCon();
+    		pwd = Hash.hash256(pwd);
+    		pstmt = connection.prepareStatement("SELECT users.username, users.password, companies.name AS company FROM users, companies WHERE username = ? AND companies.name = ?;");
     		pstmt.setString(1, username);
     		pstmt.setString(2, companyName);
     		rs = pstmt.executeQuery();
@@ -32,10 +30,11 @@ public class LoginStatements extends DBMain {
     	return false;
     }
     
-    public Set<String> getUserRoles(int userId){
+    public Set<String> getUserRoles(int userId) throws Exception{
+    	Connection connection = c.getCon();
 		Set<String> s = new HashSet<String>();
     	try{
-			PreparedStatement pstmt = c.preparedStatement("SELECT roles.role FROM roles WHERE roles.user_id = ?");
+			PreparedStatement pstmt = connection.prepareStatement("SELECT roles.role FROM roles WHERE roles.user_id = ?");
 			pstmt.setInt(1, userId);
 			rs = pstmt.executeQuery();
 			while(rs.next()){
@@ -47,9 +46,10 @@ public class LoginStatements extends DBMain {
 		return s;
     }
     
-	public User getUser(String email){
+	public User getUser(String email) throws Exception{
+		Connection connection = c.getCon();
 		try{
-			PreparedStatement pstmt = c.preparedStatement("SELECT users.id, users.username, users.company_id, roles.role FROM users, roles WHERE users.username=? AND roles.user_id=users.id");
+			PreparedStatement pstmt = connection.prepareStatement("SELECT users.id, users.username, users.company_id, roles.role FROM users, roles WHERE users.username=? AND roles.user_id=users.id");
 			pstmt.setString(1, email);
 			rs = pstmt.executeQuery();
 			if(rs.next()){
