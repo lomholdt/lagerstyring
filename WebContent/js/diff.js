daceb6c77ba06239f90c491f53a014fba7da707b
jonaslomholdt <jonaslomholdt@gmail.com>
2015-05-13 19:33:08 +0200

    Diffen på close siden vises altid

--- WebContent/js/diff.js
+++ WebContent/js/diff.js
@@ -3,21 +3,49 @@ function setDiff(){
 	var closeInput = mainInput.getElementsByClassName("close-input");
 	
 	for (var i = 0; i < closeInput.length; i++){
-		closeInput[i].onkeyup = function(){
-			var inputValue = this.value;
-			var expectedUnits = document.getElementById(this.id + "-inventory-expected").value;
-			
-			var diffCounter = document.getElementById(this.id + "-diff");
-			if(inputValue == ''){
-				var result = "";
-			}
-			else{
-				var result = inputValue - expectedUnits;
-			}
-			diffCounter.value = result;			
-		}
+		calc(closeInput[i]);
+		closeInput[i].onkeyup = calcThis;
+
 	}
 }
+
+
+function calc(event){
+	var inputValue = event.value;
+	
+	if(inputValue == undefined) return;
+	
+	var expectedUnits = document.getElementById(event.id + "-inventory-expected").value;
+	
+	var diffCounter = document.getElementById(event.id + "-diff");
+	if(inputValue == ''){
+		var result = "";
+	}
+	else{
+		var result = inputValue - expectedUnits;
+	}
+	diffCounter.value = result;			
+}
+
+
+
+function calcThis(){
+	var inputValue = this.value;
+	
+	if(inputValue == undefined) return;
+	
+	var expectedUnits = document.getElementById(this.id + "-inventory-expected").value;
+	
+	var diffCounter = document.getElementById(this.id + "-diff");
+	if(inputValue == ''){
+		var result = "";
+	}
+	else{
+		var result = inputValue - expectedUnits;
+	}
+	diffCounter.value = result;			
+}
+
 
 
 $(document).ready(function(){	
--- WebContent/js/save.js
+++ WebContent/js/save.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
 	//var url = $(location).attr('pathname');
 	$("#save").click(function(){
 		var id = $("input[name=sid]").val();
-		console.log(id);
 		var myData = $("#closeStorageForm").serialize();
 		myData += "&save=true";
-		console.log(myData);
 		$.post("/lagerstyring/save",
 				myData,
 				function(data, textStatus){
